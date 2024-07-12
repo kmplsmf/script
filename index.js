77cfdad4e884fd152197b6aa59ed27db647c91bb
kmplsmf <sasaalekseev@gmail.com>
2024-07-12 16:07:53 +0300

    bb

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ async function main() {
     port: process.env.PG_PORT,
   });
 
+
   const maxId = (await pool.query(`SELECT COALESCE(MAX(orderid), 0) AS max_id FROM public.${process.env.TARGET_TABLE};`)).rows[0].max_id;
   console.log(`Последний order_id: ${maxId}`);
 
