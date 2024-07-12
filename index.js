fc0fd8a74d20159bb4653c6cc7fa9383812f2bab
Artem Selifonov <selifonov.artem@mail.ru>
2024-07-12 12:55:13 +0300

    Переписал код

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
-const {Pool} = require('pg');
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'postgres',
-  port: 5433,
-});
+const {Pool} = require("pg");
+require("dotenv").config({path: __dirname + "/.env"});
 
-pool.query('SELECT coalesce(max(orderid), 0) as max_id FROM public.result_1;', function (error, results, fields) {
+async function main() {
+  const pool = new Pool({
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+  });
 
-  if (error) console.log(error);
-  console.log(results)
-  const maxOrderID = results.rows[0].max_id
-  console.log(results)
-  console.log('Самое большое число из колонки orderid: ', maxOrderID);
-  someOtherFunction(maxOrderID);
+  const maxId = (await pool.query(`SELECT COALESCE(MAX(orderid), 0) AS max_id FROM public.${process.env.TARGET_TABLE};`)).rows[0].max_id;
+  console.log(`Последний order_id: ${maxId}`);
 
-});
+  const orderList = await (await fetch(`http://gs.naukanet.ru/api/nextgis/orders/get-prorabotki-list?last_order_id=${maxId}`)).json();
 
+  let stmt = `INSERT INTO ${process.env.TARGET_TABLE} (orderid, ordertypeid, descr, lat, lon, orderurl) VALUES ($1, $2, $3, $4, $5, $6)`;
 
-async function someOtherFunction(orderID) {
-  console.log('kek')
-  const response = await fetch('http://gs.naukanet.ru/api/nextgis/orders/get-prorabotki-list');
-  const data = await response.json();
-  console.log(data)
-  let stmt = 'INSERT INTO result_1 (orderid, ordertypeid, descr, lat, lon, orderurl) VALUES ($1, $2, $3, $4, $5, $6)';
-
-  for (let i = 0; i < data.length; i++) {
-    let row = data[i];
-    await pool.query(stmt, [row.order_id, row.order_type_id, row.description, row.latitude, row.longitude, row.order_url]);
+  for (const order of orderList) {
+    await pool.query(stmt, [order.order_id, order.order_type_id, order.description, order.latitude, order.longitude, order.order_url]);
   }
 }
+
+main();
